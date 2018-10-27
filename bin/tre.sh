dc833fc587c004be097d634db72d1b4ca84073fe
Scurb <scurby@swgemu.com>
2018-10-27 09:53:27 -0400

    fix for engine sub-module

---
+++ bin/tre.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# tre.sh
+#
+
+cd
+
+#if [ -d tre ]; then
+#  mkdir tre
+#  else
+#fi
+
+cd ~/workspace/tre/
+
+for i in https://www.dropbox.com/s/mrwhhmshbayxg3d/bottom.tre https://www.dropbox.com/s/od7zgwh1ity6w6q/data_animation_00.tre https://www.dropbox.com/s/3z50r6k2gi3nebe/data_music_00.tre https://www.dropbox.com/s/fa1jzk8zwmyxgkq/data_other_00.tre https://www.dropbox.com/s/o7ru8gs8fqlyzdm/data_sample_00.tre https://www.dropbox.com/s/aaccwx40uymzwsq/data_sample_01.tre https://www.dropbox.com/s/zq2js4shxwmx2zs/data_sample_02.tre https://www.dropbox.com/s/i9hyw5vipmx2584/data_sample_03.tre https://www.dropbox.com/s/33m7vd69o7aor74/data_sample_04.tre https://www.dropbox.com/s/a7ze9eqcg6884j9/data_skeletal_mesh_00.tre https://www.dropbox.com/s/kxboyuwvt4r6g0r/data_skeletal_mesh_01.tre https://www.dropbox.com/s/dqdohticx5gqi8x/data_sku1_00.tre https://www.dropbox.com/s/78x9zljrmu8ifts/data_sku1_01.tre https://www.dropbox.com/s/tt7mh5ime7l4ltt/data_sku1_02.tre https://www.dropbox.com/s/ewyvf8z2nk446jz/data_sku1_03.tre https://www.dropbox.com/s/0fkt1ut8sbwo4ap/data_sku1_04.tre https://www.dropbox.com/s/db3cuw59916rmp2/data_sku1_05.tre https://www.dropbox.com/s/4l4zhogflwd5lca/data_sku1_06.tre https://www.dropbox.com/s/8hulyowu4exiflz/data_sku1_07.tre https://www.dropbox.com/s/5mfau1gr8b9krxv/data_static_mesh_00.tre https://www.dropbox.com/s/gmv2rv8q9f2drx3/data_static_mesh_01.tre https://www.dropbox.com/s/sovdw9g5nmmyxgq/data_texture_00.tre https://www.dropbox.com/s/4g6o49mz4ys83uz/data_texture_01.tre https://www.dropbox.com/s/0ew251d2nnbxtq2/data_texture_02.tre https://www.dropbox.com/s/hcnukwny9fr8i3x/data_texture_03.tre https://www.dropbox.com/s/gv3chawrcm44i1v/data_texture_04.tre https://www.dropbox.com/s/967pac1g7lusp7j/data_texture_05.tre https://www.dropbox.com/s/kh06xccn9mx9605/data_texture_06.tre https://www.dropbox.com/s/9a40lc854x13pyr/data_texture_07.tre https://www.dropbox.com/s/jsgxotus4d8i44i/default_patch.tre https://www.dropbox.com/s/a9rj03h8tbo2wkc/hotfix_13_1_00.tre https://www.dropbox.com/s/k2955xf3wq39wh6/hotfix_sku1_12_1_00.tre https://www.dropbox.com/s/tf2nhwtlyvfgj5i/patch_00.tre https://www.dropbox.com/s/d65mg7s7d19hmdm/patch_01.tre https://www.dropbox.com/s/wdjzmbsnwh41rmo/patch_02.tre https://www.dropbox.com/s/fhmiplf0jl4grk7/patch_03.tre https://www.dropbox.com/s/4hkhiwiidmky0te/patch_04.tre https://www.dropbox.com/s/7vq54e6efk594h7/patch_05.tre https://www.dropbox.com/s/opxnl1f9xnbpqos/patch_06.tre https://www.dropbox.com/s/hj3097t0wy3rlob/patch_07.tre https://www.dropbox.com/s/x3pl1teuthu6zs4/patch_08.tre https://www.dropbox.com/s/6aaa2r5stcoy1i9/patch_09.tre https://www.dropbox.com/s/yq5t60k94mh36ng/patch_10.tre https://www.dropbox.com/s/tvj42y0t3mcvvuu/patch_11_00.tre https://www.dropbox.com/s/xjl10p5zot7el1s/patch_11_01.tre https://www.dropbox.com/s/r8xyn6o2qv5xldy/patch_11_02.tre https://www.dropbox.com/s/ho1zaybhfol474g/patch_11_03.tre https://www.dropbox.com/s/v46hb6a2fhzeaob/patch_12_00.tre https://www.dropbox.com/s/bj1n7qz1jurrf4i/patch_13_00.tre https://www.dropbox.com/s/p9pilqox31ei079/patch_14_00.tre https://www.dropbox.com/s/wrcmicf8rpojfjh/patch_sku1_12_00.tre https://www.dropbox.com/s/zbkzd7e1dzdq1tp/patch_sku1_13_00.tre https://www.dropbox.com/s/ng546tlxzj3kugb/patch_sku1_14_00.tre https://www.dropbox.com/s/gsq4jpp4kq3t690/swgemu_hotfix.tre
+  do
+    wget ${i}
+  done    
+
